831dcf13fc7ff29ce43b4f6e361420735d1f62ee
Maah97 <mahmoudouaboul@gmail.com>
2024-11-17 22:47:57 +0100

    szer

--- src/i18n.js
+++ src/i18n.js
@@ -3,16 +3,10 @@ import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-http-backend';
 
-export const supportedLngs = {
-    en: "English",
-    fr: "Français",
-  };
-
 i18n.use(Backend).use(LanguageDetector).use(initReactI18next).init({
     debug: true,
     fallbackLng: 'en',
     lng: 'en',
-    supportedLngs: Object.keys(supportedLngs),
     returnObjects: true,
     backend: {
       loadPath: '/locales/{{lng}}/translation.json'
