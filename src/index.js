2d0cd08dd139f01e46f10eece06e5e520e03d40b
Maah97 <mahmoudouaboul@gmail.com>
2024-11-18 12:33:45 +0100

    strs

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { React, Suspense } from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
