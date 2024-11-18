b933e1d796365aa6152fb7f9a09ce9472d7140e7
Maah97 <mahmoudouaboul@gmail.com>
2024-11-18 12:11:03 +0100

    ml

--- src/i18n.js
+++ src/i18n.js
@@ -7,5 +7,7 @@ import Backend from 'i18next-http-backend';
 i18n.use(Backend).use(LanguageDetector).use(initReactI18next).init({
     debug: true,
     fallbackLng: 'en',
+    lng: 'en',
+    returnObjects: true,
 });
 export default i18n;
