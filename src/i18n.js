d815861de5a9b4574e310ab2acc9b18ea87885e6
Maah97 <mahmoudouaboul@gmail.com>
2024-11-18 11:54:04 +0100

    zear

--- src/i18n.js
+++ src/i18n.js
@@ -1,14 +1,11 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import HttpApi from 'i18next-http-backend';
+import Backend from 'i18next-http-backend';
 
 
-i18n.use(HttpApi).use(LanguageDetector).use(initReactI18next).init({
+i18n.use(Backend).use(LanguageDetector).use(initReactI18next).init({
     debug: true,
     fallbackLng: 'en',
-    backend: {
-      loadPath: '/locales/{{lng}}/translation.json'
-  }
 });
 export default i18n;
